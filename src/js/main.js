755d0dbd2440e8f94b420ee88789c372158382e1
jw82uk <73556349+jw82uk@users.noreply.github.com>
2026-06-11 21:06:17 +0100

    Started styling

--- src/js/main.js
+++ src/js/main.js
@@ -7,5 +7,5 @@ document.addEventListener('DOMContentLoaded', () => {
   document.documentElement.classList.replace('no-js', 'has-js');
 
   // Initialize tabs component
-  initTabs('.tab-container');
+  initTabs('.tabs');
 });
